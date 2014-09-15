3d0364f3b542c61ca2d0fea277d04792d14347eb
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-15 11:59:20 -0400

    new specs

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -9,7 +9,25 @@ describe Genome do
   end
 
   it '.save' do
-    expect(genome.save).to_not be_nil
+    expect(genome.save).to_not be(nil)
   end
 
+  it '.assembly_id' do
+    expect(genome.assembly_id).to_not be(nil)
+  end
+
+end
+
+describe Scaffold do
+
+  let (:genome) { Genome.new assembly_id: 1 }
+  let (:scaffold) { Scaffold.new sequence: 'GATC', genome_id: genome.id }
+
+  it '.new' do
+    expect(scaffold).not_to be(nil)
+  end
+
+  it '.save' do
+    expect(scaffold.save).not_to be(nil)
+  end
 end
