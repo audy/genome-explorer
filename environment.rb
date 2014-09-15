7dc21be245b0e757b5f3e88c70545a6fbba733a8
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-15 16:31:26 -0400

    pull data from ncbi

--- environment.rb
+++ environment.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'json'
 
 require 'bundler'
 
--- models/genome.rb
+++ models/genome.rb
@@ -2,6 +2,10 @@ class Genome < Sequel::Model
   one_to_many :scaffolds
   one_to_many :features
 
+  def fetch_assembly_info_from_ncbi
+    resp = `bionode-ncbi search assembly #{self.assembly_id}`
+    JSON.parse(resp)
+  end
   def before_save
     self.feature_count = self.features.size
     super
