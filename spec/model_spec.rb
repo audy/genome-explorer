bbbe759bbd2c340a99e4526ecdf9c26ab6a16edf
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-15 12:02:00 -0400

    more specs

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-let(:genome) { Genome.new assembly_id: 1 }
-let (:scaffold) { Scaffold.new sequence: 'GATC', genome_id: genome.id }
 
 describe Genome do
+
+  let(:genome) { Genome.new assembly_id: 1 }
 
   it '.new' do
     expect(genome).not_to be(nil)
@@ -22,6 +22,7 @@ end
 describe Scaffold do
 
   let (:genome) { Genome.new assembly_id: 1 }
+  let (:scaffold) { Scaffold.new sequence: 'GATCGATCGATCGATC', genome_id: genome.id }
 
   it '.new' do
     expect(scaffold).not_to be(nil)
@@ -33,4 +34,16 @@ describe Scaffold do
 end
 
 describe Feature do
+  let (:genome) { Genome.new assembly_id: 1 }
+  let (:scaffold) { Scaffold.new sequence: 'GATCGATCGATCGATC', genome_id: genome.id }
+  let (:feature) { Feature.new start: 1, stop: 10, source: 'test-source', score: 0.9, info: 'test info', type: 'test-type' }
+
+  it '.new' do
+    expect(feature).to_not be(nil)
+  end
+
+  it '.save' do
+    expect(feature.save).to_not be(nil)
+  end
+
 end
