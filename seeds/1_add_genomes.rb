0eed35c3bd65eb030c53345477580b1ac7608b74
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-17 14:10:45 -0400

    faster but messier import

--- seeds/1_add_genomes.rb
+++ seeds/1_add_genomes.rb
@@ -32,15 +32,22 @@ namespace :seed do
       puts "- #{@genome.scaffolds.count} scaffolds."
 
       puts 'adding features'
+      columns = %w{start stop source strand score type info scaffold_id genome_id}.map &:to_sym
       Zlib::GzipReader.open(gff) do |handle|
-        App::DB.transaction do
-          handle.each do |line|
-            next if line[0] == '#'
-            gff_data = parse_gff_line(line).merge(scaffold: @scaffold,
-                                                  genome: @genome)
-            feature = Feature.create(gff_data)
+        # prevents procs from being stored in memory. will break hooks.
+        Feature.use_after_commit_rollback = false
+        App::DB.transaction {
+          handle.each_slice(1000) do |chunk|
+
+            dat = chunk.reject { |x| x[0] == '#' }.map do |x|
+              parse_gff_line(x).merge(scaffold_id: @scaffold.id,
+                                   genome_id: @genome.id).values_at(*columns)
+            end
+
+            App::DB[:features].import(columns, dat)
+
           end
-        end
+        }
       end
 
       @genome.save # force updating of counter cache
@@ -55,11 +62,11 @@ def parse_gff_line(line)
   line = line.strip.split("\t")
   scaffold_id = line[0].to_i
   source = line[1]
-  type = line[2].to_sym
+  type = line[2]
   start = line[3].to_i
   stop = line[4].to_i
   score = line[5].to_f
-  strand = line[6] == '+' ? :forward : :reverse 
+  strand = line[6] == '+' ? 'forward' : 'reverse'
   info = line[8]
 
   { start: start,
