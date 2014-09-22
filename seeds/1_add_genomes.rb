b8877f730ddabae748273439e59d273ffdb81ee2
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-22 12:21:24 -0400

    coerce to string

--- seeds/1_add_genomes.rb
+++ seeds/1_add_genomes.rb
@@ -76,15 +76,15 @@ def parse_gff_line(line)
 
   fields = line.strip.split("\t")
 
-  scaffold_name = fields[0]
-  source        = fields[1]
-  type          = fields[2]
+  scaffold_name = String fields[0]
+  source        = String fields[1]
+  type          = String fields[2]
   start         = Integer fields[3] rescue nil
   stop          = Integer fields[4] rescue nil
   score         = Float fields[5] rescue nil
-  strand        = fields[6]
+  strand        = String fields[6]
   frame         = Integer fields[7] rescue nil
-  info          = fields[8]
+  info          = String fields[8]
 
   { scaffold_name: scaffold_name,
     start: start,
