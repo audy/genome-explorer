210d1fa851476277b8d574ad3d09120fbe7fbda8
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-22 12:20:25 -0400

    more reliable feature string extraction

--- models/feature.rb
+++ models/feature.rb
@@ -1,12 +1,20 @@
+class String
+  def complement
+    self.tr('GATCgatc', 'CTAGctag')
+  end
+end
+
 class Feature < Sequel::Model
   many_to_one :scaffold
   many_to_one :genome
 
   def sequence
-    frame = frame.nil? ? 0 : frame
-    i =  -1 + self.start + frame
-    j = -1 + self.stop + frame
+    i =  -1 + self.start
+    j = -1 + self.stop
     seq =  self.scaffold.sequence[i..j]
+    if strand == '-'
+      seq = seq.reverse.complement
+    end
     seq
   end
 end
