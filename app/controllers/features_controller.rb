e6191d0a624896830b8a09d83e367fb279cc936c
audy <harekrishna@gmail.com>
2014-09-25 16:55:30 -0400

    changed show, added related features

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -2,6 +2,7 @@ class FeaturesController < ApplicationController
 
   def show
     @feature = Feature.find params[:id]
+    @related_features = @feature.related_features.where(feature_type: 'CDS')
   end
 
 end
