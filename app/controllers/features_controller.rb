00557efeba153163ea8401f6956d9f3acf08e340
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-06 11:15:39 -0400

    hiding similar proteins until science happens

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -2,7 +2,7 @@ class FeaturesController < ApplicationController
 
   def show
     @feature = Feature.find params[:id]
-    related = @feature.find_similar_proteins
+    related = Hash.new #@feature.find_similar_proteins
     @related_features = related.keys.map { |x| Feature.find(x) }.compact
   end
 
