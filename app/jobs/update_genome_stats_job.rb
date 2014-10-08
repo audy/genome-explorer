e70c5583ed28e17002da9e7294e4881d621326e6
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-08 17:19:43 -0400

    fixed update genome stats job

--- app/jobs/update_genome_stats_job.rb
+++ app/jobs/update_genome_stats_job.rb
@@ -2,16 +2,13 @@ UpdateGenomeStatsJob = Struct.new(:id) do
 
   def perform
     @genome = Genome.find(self.id)
-    # update summary statistics for a genome, storing results in HStore column :stats
-    def update_stats
-      @genome[:stats] = { total_features: @genome.features.count,
-                      total_scaffolds: @genome.scaffolds.count,
-                      genome_size: @genome.scaffolds.map { |x| x.sequence.size }.inject(:+),
-                      total_proteins: @genome.features.where(feature_type: 'CDS').count,
-                      shared_proteins: @genome.features.where(feature_type: 'CDS').count{ |x| x.related_features.size > 1 ? 1 : 0 }
-      }
-      @genome.save
-    end
+    @genome[:stats] = { total_features: @genome.features.count,
+                    total_scaffolds: @genome.scaffolds.count,
+                    genome_size: @genome.scaffolds.map { |x| x.sequence.size }.inject(:+),
+                    total_proteins: @genome.features.where(feature_type: 'CDS').count,
+                    shared_proteins: @genome.features.where(feature_type: 'CDS').count{ |x| x.related_features.size > 1 ? 1 : 0 }
+    }
+    @genome.save
   end
 
 end
