e7ab65998a083d40abbb20ce99ae38690e531602
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-10 13:03:28 -0400

    added ability to specify min_shared

--- app/controllers/genome_relationships_controller.rb
+++ app/controllers/genome_relationships_controller.rb
@@ -5,6 +5,8 @@ class GenomeRelationshipsController < ApplicationController
     respond_to do |format|
       format.json do
 
+        @min_related = Integer(params[:min_related] || 0)
+
         genome_relationships = GenomeRelationship.all
         genomes = Genome.all
 
@@ -12,14 +14,19 @@ class GenomeRelationshipsController < ApplicationController
 
         nodes = genomes.map do |genome|
           genomes_list << genome.id
-          { name: genome.organism, group: genome.organism.split.first }
+          { name: genome.organism, group: genome.organism.split[1] }
         end
 
         index = Hash[genomes_list.map.with_index.to_a] 
 
         links = genome_relationships.map do |rel|
-          { source: index[rel.genome_id], target: index[rel.related_genome_id], value: rel.related_features_count }
-        end
+          count = rel.related_features_count
+          if count > @min_related
+            { source: index[rel.genome_id], target: index[rel.related_genome_id], value: Math.sqrt(count) }
+          else
+            nil
+          end
+        end.compact
 
         dat = { nodes: nodes, links: links }
 
