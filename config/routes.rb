14bc3df83accb265d122001e0b29677bbf039afb
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-10 11:29:18 -0400

    added genome graph to routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
     resources :features
   end
 
+  resources :genome_relationships do
+  end
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
