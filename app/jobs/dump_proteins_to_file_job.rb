231291af7d30f55bebda75fc2a5aa77a6dab9164
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-14 21:01:04 -0400

    use find_each, doesnt really improve speed though

--- app/jobs/dump_proteins_to_file_job.rb
+++ app/jobs/dump_proteins_to_file_job.rb
@@ -5,7 +5,7 @@ DumpProteinsToFileJob = Struct.new(:filename) do
 
     pbar = ProgressBar.new 'dumping', Feature.where(feature_type: 'CDS').count
 
-    Scaffold.all.each do |scaffold|
+    Scaffold.find_each do |scaffold|
       scaffold.features.where(feature_type: 'CDS').each do |feature|
         seq = feature.protein_sequence
         # skip proteins w/ weird starts or stops in the middle or dont end in a
