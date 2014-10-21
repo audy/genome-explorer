cda5aebd4ab5c8cb4be3c1616fd18e64f575abb1
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-20 23:40:34 -0400

    dont do random

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -73,7 +73,8 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = :random
+  # (this is a bad way to test)
+  # config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
