a09cace92a91064eaeeba657abd4ada17263ec8c
Austin Davis-Richardson <harekrishna@gmail.com>
2014-11-04 12:02:45 -0500

    skip tests if DB connection fails

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,11 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+unless ActiveRecord::Base.connected?
+  $stderr.puts 'ERROR! cannot connect to database'
+  exit -1
+end
+
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
