da9082fbb021b63888210c8f8151fbe22989401e
Austin Davis-Richardson <harekrishna@gmail.com>
2014-11-04 22:47:17 -0500

    faster dumping through chacing

--- app/jobs/dump_proteins_to_file_job.rb
+++ app/jobs/dump_proteins_to_file_job.rb
@@ -5,14 +5,23 @@ DumpProteinsToFileJob = Struct.new(:filename) do
 
     tot = 0
 
-    Scaffold.find_each do |scaffold|
-      scaffold.features.where(feature_type: 'CDS').each do |feature|
+    pbar = ProgressBar.new 'dumping', Feature.where(feature_type: 'CDS').count
+
+    Scaffold.cache {
+      Feature.where(feature_type: 'CDS').find_each do |feature|
+        pbar.inc
         # skip proteins w/ weird starts or stops in the middle or dont end in a
         # stop.
         out.puts ">#{feature.id}\n#{feature.protein_sequence}" unless feature.weird?
         tot += 1
       end
-    end
+    }
+
+    pbar.finish
+
+    fail 'no proteins' unless tot > 0
+
+    puts "wrote #{tot} proteins"
 
     out.close
 
