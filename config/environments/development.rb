47a41226cd8b46d6e4dbd11ec827ab59be924e72
Austin Davis-Richardson <harekrishna@gmail.com>
2014-12-01 13:31:01 -0500

    include rack::livereload middleware

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # autoreload page w/ Guard
+  config.middleware.use Rack::LiveReload # (inject some javascript)
 end
