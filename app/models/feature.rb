0ac99b87bc025953c15269916f638d9b2de74364
Austin Davis-Richardson <harekrishna@gmail.com>
2015-01-14 12:00:10 -0500

    removed trailing whitespace

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -4,6 +4,26 @@ class String
   end
 end
 
+# copy/pasted from https://github.com/bioruby/bioruby/blob/5f3569faaf89ebcd2b2cf9cbe6b3c1f0544b2679/lib/bio/data/codontable.rb
+CODON_TABLE = {
+      'ttt' => 'F', 'tct' => 'S', 'tat'	=> 'Y', 'tgt' => 'C',
+      'ttc' => 'F', 'tcc' => 'S', 'tac'	=> 'Y', 'tgc' => 'C',
+      'tta' => 'L', 'tca' => 'S', 'taa'	=> '*', 'tga' => '*',
+      'ttg' => 'L', 'tcg' => 'S', 'tag'	=> '*', 'tgg' => 'W',
+      'ctt' => 'L', 'cct' => 'P', 'cat'	=> 'H', 'cgt' => 'R',
+      'ctc' => 'L', 'ccc' => 'P', 'cac'	=> 'H', 'cgc' => 'R',
+      'cta' => 'L', 'cca' => 'P', 'caa'	=> 'Q', 'cga' => 'R',
+      'ctg' => 'L', 'ccg' => 'P', 'cag'	=> 'Q', 'cgg' => 'R',
+      'att' => 'I', 'act' => 'T', 'aat'	=> 'N', 'agt' => 'S',
+      'atc' => 'I', 'acc' => 'T', 'aac'	=> 'N', 'agc' => 'S',
+      'ata' => 'I', 'aca' => 'T', 'aaa'	=> 'K', 'aga' => 'R',
+      'atg' => 'M', 'acg' => 'T', 'aag'	=> 'K', 'agg' => 'R',
+      'gtt' => 'V', 'gct' => 'A', 'gat'	=> 'D', 'ggt' => 'G',
+      'gtc' => 'V', 'gcc' => 'A', 'gac'	=> 'D', 'ggc' => 'G',
+      'gta' => 'V', 'gca' => 'A', 'gaa'	=> 'E', 'gga' => 'G',
+      'gtg' => 'V', 'gcg' => 'A', 'gag'	=> 'E', 'ggg' => 'G',
+}
+
 class Feature < ActiveRecord::Base
   belongs_to :scaffold
   belongs_to :genome
@@ -39,7 +59,9 @@ class Feature < ActiveRecord::Base
     # create a new nucleotide sequence, translate it.
     # do NOT use 'auto' as it will sometimes mistake nucleotide for
     # amino acid
-    Bio::Sequence::NA.new(self.sequence).translate # remove stop codon
+    self.sequence.downcase.chars.each_slice(3).map do |sl|
+      CODON_TABLE[sl.join]
+    end.join
   end
 
   def product
