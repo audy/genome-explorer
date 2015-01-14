71c4cbaf183f401f75ad5ac3619044ca97dee212
Austin Davis-Richardson <harekrishna@gmail.com>
2015-01-13 23:46:43 -0500

    default queue is default

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# set default Delayed Job queue
+
+Delayed::Worker.default_queue_name = 'default'
+
 # Initialize the Rails application.
 Rails.application.initialize!
