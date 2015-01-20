d5b0ecb2f82435bae18d5079d97e15377bb5dea8
Austin Davis-Richardson <harekrishna@gmail.com>
2015-01-20 12:14:59 -0500

    appears to only import new relationships (but slow)

--- app/jobs/find_related_proteins_job.rb
+++ app/jobs/find_related_proteins_job.rb
@@ -1,7 +1,9 @@
+require 'set'
 
 # 1. run usearch
 # 2. parse blast-6-tab output
-# 3. Create new entries in the feature relationships graph
+# 3. Add entries in blast-6-tab output to FeatureRelationship table
+#     - doesn't skip existing relationships
 
 class FindRelatedProteinsJob
 
@@ -58,25 +60,38 @@ class FindRelatedProteinsJob
   def build_relationships_from_blast_output
     # todo allow for multiple types of proteinrelationships from different
     # sources.
-    # todo allow for filtering criteria using `filtration_lambda` (forgive me)
-    # which is a lambda that will result in hits being filtered out if it
-    # evaluates to false
+    
+    # keep a list of existing protein relationships and don't re-import them
+    # can't rely on ActiveRecord to do this because we're using `import`.
+    puts 'generating list of existing protein relationships'
+    existing = ProteinRelationship.find_each.map { |x| [x.feature_id, x.related_feature_id ]}
+    existing = existing.to_set
+    
+    puts "found #{existing.size} existing protein relationships"
+    
+    total_imported = 0
+    
     File.open(@output) do |handle|
       pbar = ProgressBar.new 'loading', File.size(handle.path)
       columns = [ :feature_id, :related_feature_id, :identity ]
       read_blast_file(handle).each_slice(10_000) do |values|
         pbar.set handle.pos
+        # (this part should be working now)
+        values.reject! { |v| existing.include? v[0..1] }
+        total_imported += values.size
         ProteinRelationship.import columns, values, validate: false
       end
       pbar.finish
     end
+    
+    puts "imported #{total_imported} new protein relationships"
   end
 
   def parse_blast_line line
     fields = line.strip.split("\t")
     # query, subject
-    [ fields[0], # query id
-      fields[1], # subject id
+    [ Integer(fields[0]), # query id
+      Integer(fields[1]), # subject id
       (100 * Float(fields[2])).to_i # percent identity, rounded
     ]
   end
