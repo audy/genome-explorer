e03e7ce0dcf808fc33681c54776d445822a909f2
Austin Davis-Richardson <harekrishna@gmail.com>
2015-01-22 17:18:52 -0500

    mocked specs for genome relationships pipeline job

---
+++ spec/jobs/update_genome_relationships_pipeline_job_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe UpdateGenomeRelationshipsPipelineJob do
+
+  it 'builds the genome relationships pipeline from scratch'
+
+  it 'can update the genome relationships graph with the addition of a new genome'
+
+  it 'the updated version of the graph is equivalent to the graph built form scratch'
+
+  it 'only creates *new* feature relationships'
+
+  it 'only creates *new* genome relationships'
+
+end
