f3df44897175c1ae463d9eca98065ed4218b9ee3
Austin Davis-Richardson <harekrishna@gmail.com>
2018-04-08 14:26:40 -0700

    rm rspec focus

--- spec/jobs/fetch_genomes_from_ncbi_job_spec.rb
+++ spec/jobs/fetch_genomes_from_ncbi_job_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe FetchGenomesFromNCBIJob, focus: true do
+describe FetchGenomesFromNCBIJob do
 
   # use a local, truncated assembly summaries file so 50k genomes are not
   # downloaded and created
