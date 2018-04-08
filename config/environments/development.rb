60e087898607a4710e3926e6049ddba4c8526568
Austin Davis-Richardson <harekrishna@gmail.com>
2018-04-08 12:15:36 -0700

    removes livereload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
-  # autoreload page w/ Guard
-  config.middleware.use Rack::LiveReload # (inject some javascript)
 end
